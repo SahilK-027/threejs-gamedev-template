821cb942d3a8d22ad693130410f8bbd837a58e1d
Sahil Kandhare <skandhare_comp@jspmrscoe.edu.in>
2026-01-20 13:46:51 +0530

    🐞 fix: this fixes issue #1

--- src/Game/Utils/Time.class.js
+++ src/Game/Utils/Time.class.js
@@ -7,23 +7,20 @@ export default class Time extends EventEmitter {
     this.start = Date.now();
     this.current = this.start;
     this.elapsedTime = 0;
-    this.delta = 34;
+    this.delta = 0;
+    this.maxDelta = 0.05;
 
-    window.requestAnimationFrame(() => {
-      this.animate();
-    });
+    window.requestAnimationFrame(() => this.animate());
   }
 
   animate() {
     const currentTime = Date.now();
-    this.delta = (currentTime - this.current) / 1000;
+    this.delta = Math.min((currentTime - this.current) / 1000, this.maxDelta);
     this.current = currentTime;
     this.elapsedTime = (this.current - this.start) / 1000;
 
     this.trigger('animate');
 
-    window.requestAnimationFrame(() => {
-      this.animate();
-    });
+    window.requestAnimationFrame(() => this.animate());
   }
 }
